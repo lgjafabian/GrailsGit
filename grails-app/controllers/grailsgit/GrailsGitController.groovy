bec4371e02715915ea1665e93bd3c532d79b348d
santi0037 <santiagomoranlabat@gmail.com>
2018-03-05 14:33:54 -0300

    view sumar

--- grails-app/controllers/grailsgit/GrailsGitController.groovy
+++ grails-app/controllers/grailsgit/GrailsGitController.groovy
@@ -1,6 +1,18 @@
 package grailsgit
 
-class GrailsGitController {
+import static org.springframework.http.HttpStatus.NO_CONTENT
 
-    def index() { }
+class GrailsGitController {
+    CategoryService categoryService
+
+
+    def sumar(Long id) {
+        def cat = categoryService.get(id)
+        cat.setVisits(cat.visits + 1)
+        categoryService.save(cat)
+        println "a"
+        [visitas: cat.visits]
+    }
+
+
 }
