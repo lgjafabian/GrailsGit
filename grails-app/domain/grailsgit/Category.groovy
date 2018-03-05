502147f1677bf425dfeea565159a1877388cc7da
svarela <sofiamarielavarelamerino@gmail.com>
2018-03-05 13:38:11 -0300

    sofi-branch

--- grails-app/domain/grailsgit/Category.groovy
+++ grails-app/domain/grailsgit/Category.groovy
@@ -1,17 +1,18 @@
 package grailsgit
 
 class Category {
-
+// sofi esta aca
     String name
     static belongsTo= [site: Site]
 
-    //punto b
+    //punto b sdljvcbdhsajlvdglchsdjchvgbckzdn<bc ,dhjv,kbchchkdhjkshkuwehfcajksulbiwyO9GCYBHKIRUCBLIwerui
     String urlImage
     Integer visits
 
-    //agrego los constraints
+    //agrego los constraints por 2 vez
     static constraints = {
         name blank: false, nullable: false
         urlImage blank: false, nullable: false
+        visits blank: false
     }
 }
