1b52854f2c75cf6f6c232a405994c926c535f9b7
svarela <sofiamarielavarelamerino@gmail.com>
2018-03-05 13:19:22 -0300

    Modifique constraints

--- grails-app/domain/grailsgit/Category.groovy
+++ grails-app/domain/grailsgit/Category.groovy
@@ -3,9 +3,15 @@ package grailsgit
 class Category {
 
     String name
-
     static belongsTo= [site: Site]
 
+    //punto b
+    String urlImage
+    Integer visits
+
+    //agrego los constraints
     static constraints = {
+        name blank: false, nullable: false
+        urlImage blank: false, nullable: false
     }
 }
